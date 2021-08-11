140655cdbc1c049d880bd2a0228cd3da56b4aab5
AleksMMM <gol8an@yandex.ru>
2021-08-11 18:27:53 +0300

    исправленная версия

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -5,31 +5,18 @@ import static java.lang.Math.pow;
 
 public class Point {
 
-    private int x1;
-    private int y1;
-    private int x2;
-    private int y2;
 
-
-    public Point(int x1, int x2, int y1, int y2) {
-        this.x1 = x1;
-        this.x2 = x2;
-        this.y1 = y1;
-        this.y2 = y2;
-    }
-
-
-    public double distance(Point point) {
-        double calc = Math.pow((point.x2 - point.x1), 2) + Math.pow((point.y2 - point.y1), 2);
+    public double distance(int x1, int x2, int y1, int y2) {
+        double calc = Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2);
         return Math.sqrt(calc);
     }
 
 
 
     public static void main(String[] args) {
-        Point a = new Point(0, 0, 2, 0);
+        Point a = new Point();
 
-        System.out.println("result (0, 0) to (2, 0) " + a.distance(a));
+        System.out.println("result (0, 0) to (2, 0) " + a.distance(0, 0,2, 0));
 
     }
 
