35815aadcf0cd42f5c31c064d25a42e513926874
AleksMMM <gol8an@yandex.ru>
2021-08-16 15:31:14 +0300

    Тесты для определения расстояния между точками, исправленные

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -8,10 +8,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class PointTest {
 
     @Test
-    void distance() {
-        Point point = new Point(0, 1, 2, 1);
+    void when01to21then14() {
+        Point point = new Point();
         double expected = 1.41;
-        double out = point.distance(point);
+        double out = point.distance(0, 1, 2, 1);
+        double delta = 0.01;
+        Assert.assertEquals(expected, out, delta);
+
+    }
+
+    @Test
+    void when00to20then2() {
+        Point point = new Point();
+        double expected = 2;
+        double out = point.distance(0, 0, 2, 0);
+        double delta = 0.01;
+        Assert.assertEquals(expected, out, delta);
+
+    }
+
+    @Test
+    void when00to30then3() {
+        Point point = new Point();
+        double expected = 3;
+        double out = point.distance(0, 0, 3, 0);
         double delta = 0.01;
         Assert.assertEquals(expected, out, delta);
 
