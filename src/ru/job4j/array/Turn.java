6cb0cf4b9d5429f8956c1b2956528e633c61ed13
AleksMMM <gol8an@yandex.ru>
2021-08-23 14:26:15 +0300

    переделал

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -6,11 +6,12 @@ import java.util.List;
 
 public class Turn {
     public static int[] back(int[] nums) {
-        int[] reversed = new int[nums.length];
-        for (int i=0; i<nums.length; i++) {
-            reversed[i] = nums[nums.length - 1 - i];
+        for (int i = 0; i < nums.length / 2; i++) {
+            int tmp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = tmp;
         }
-        return reversed;
+        return nums;
     }
 
 }
--- test/ru/job4j/array/TurnTest.java
+++ test/ru/job4j/array/TurnTest.java
@@ -17,6 +17,11 @@ class TurnTest {
 
     @Test
     public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+
+        int[] input = new int[] {1, 2, 3, 4, 5};
+        int[] result = Turn.back(input);
+        int[] expected = new int[] {5, 4, 3, 2, 1};
+        Assert.assertArrayEquals(expected, result);
         /*
             напишите здесь тест, проверяющий переворот массива
             с нечётным числом элементов, например {1, 2, 3, 4, 5}.
