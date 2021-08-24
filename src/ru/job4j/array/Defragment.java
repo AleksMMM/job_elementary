c70461c2ece1e1b0eff5556c03e79fa20a31130c
AleksMMM <gol8an@yandex.ru>
2021-08-24 13:45:16 +0300

    Дефрагментация массива

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,38 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        int nullIndex = 0;
+        int notNullIndex = 0;
+
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                nullIndex = index;
+                for (int i = index + 1; i < array.length; i++) {
+                    if (array[i] != null) {
+                        notNullIndex = i;
+                        swap(array, nullIndex, notNullIndex);
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    private static void swap(String[] array, int nullIndex, int notNullIndex) {
+        String temp = "";
+        temp = array[nullIndex];
+        array[nullIndex] = array[notNullIndex];
+        array[notNullIndex] = temp;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
