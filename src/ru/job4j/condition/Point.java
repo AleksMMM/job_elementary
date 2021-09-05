4f5bffd6811d8eaf81647d70aa4b875b90ebd618
AleksMMM <gol8an@yandex.ru>
2021-09-05 12:48:44 +0300

    Рефакторинг - Расстояние между точками.

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -5,18 +5,28 @@ import static java.lang.Math.pow;
 
 public class Point {
 
+    private int x;
+    private int y;
 
-    public double distance(int x1, int x2, int y1, int y2) {
-        double calc = Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2);
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public double distance(Point point) {
+        double calc = Math.pow((this.x - point.x), 2) + Math.pow((this.y - point.y), 2);
         return Math.sqrt(calc);
     }
 
 
 
     public static void main(String[] args) {
-        Point a = new Point();
+        Point a1 = new Point(0, 0);
+        Point a2 = new Point(2, 0);
 
-        System.out.println("result (0, 0) to (2, 0) " + a.distance(0, 0,2, 0));
+
+        System.out.println("result (0, 0) to (2, 0) " + a1.distance(a2));
 
     }
 
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -9,9 +9,10 @@ class PointTest {
 
     @Test
     void when01to21then14() {
-        Point point = new Point();
-        double expected = 1.41;
-        double out = point.distance(0, 1, 2, 1);
+        Point point = new Point(0,1);
+        Point point2 = new Point(2,1);
+        double expected = 2.0;
+        double out = point.distance(point2);
         double delta = 0.01;
         Assert.assertEquals(expected, out, delta);
 
@@ -19,9 +20,10 @@ class PointTest {
 
     @Test
     void when00to20then2() {
-        Point point = new Point();
+        Point point = new Point(0,0);
+        Point point2 = new Point(2,0);
         double expected = 2;
-        double out = point.distance(0, 0, 2, 0);
+        double out = point.distance(point2);
         double delta = 0.01;
         Assert.assertEquals(expected, out, delta);
 
@@ -29,9 +31,10 @@ class PointTest {
 
     @Test
     void when00to30then3() {
-        Point point = new Point();
+        Point point = new Point(0,0);
+        Point point2 = new Point(3,0);
         double expected = 3;
-        double out = point.distance(0, 0, 3, 0);
+        double out = point.distance(point2);
         double delta = 0.01;
         Assert.assertEquals(expected, out, delta);
 
