9c1c31a016d5682c94d00e7d2ca5f6e8e4c1b358
AleksMMM <gol8an@yandex.ru>
2021-09-06 18:19:44 +0300

    исправил

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -7,13 +7,12 @@ public class Max {
     }
 
     public static int max(int left, int right, int three) {
-        int result = max(left, right) > three ? max(left, right) : three;
-        return result;
+        return max(left, max(right, three));
     }
 
     public static int max(int left, int right, int three, int four) {
-        int result = max(left, right, three) > four ? max(left, right, three) : four;
-        return result;
+
+        return max(left, max(right, three, four));
     }
 
 }
